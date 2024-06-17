b1d96c56094b458a6ef09b04a6687e27e66cdf11
pavithraj0809 <pavithraj0809@gmail.com>
2024-06-17 12:59:46 +0530

    code changed

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const router = require('./router/router');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api", router);
+
+mongoose.connect('mongodb+srv://pavithra:pavi@cluster0.r1waizr.mongodb.net/one')
+  .then(() => console.log("Database Connected"))
+  .catch((error) => console.log("Error in DB Connection", error));
+
+module.exports = app;
